f54e26323a6e9f975194c85c7c2eeff54020160c
sdelehoy <sdelehoy@icloud.com>
2023-06-20 16:05:31 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ const direction = ["right", "left", "straight"]
 const random = Math.floor(Math.random() * direction.length)
 
 function writeDirection() {
-  document.getElementsByClassName("direction-text").innerHTML = direction[random]
+  return document.getElementsByClassName("direction-text").innerHTML = direction[random]
 }
